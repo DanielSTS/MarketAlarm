26a470961e48fc4d266f458a8ce1e4ff791b133d
Daniel Júnior <daniel.junior.ifpb@gmail.com>
2023-06-16 21:22:09 -0300

    feat: add folders and modify main

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 from flask import Flask
+from interfaces import routes
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+routes.init_app(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
